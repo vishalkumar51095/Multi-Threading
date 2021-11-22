56b679d67ee421de28be761dd6b49b5a6526d085
Shivank66 <shivankbhardwajme@gmail.com>
2021-11-22 18:55:13 +0530

    multiClient

--- Game/GameClient.java
+++ Game/GameClient.java
@@ -7,14 +7,40 @@ public class GameClient
     public static void main(String[] args) throws IOException {
         int portno=1000;
         Socket s=new Socket("localhost", portno);
+        PrintWriter pw=new PrintWriter(s.getOutputStream());
     BufferedReader br=new BufferedReader(new InputStreamReader( s.getInputStream()));
+    ReadThread rt=new ReadThread(br);
+    rt.start();
+    Scanner s1=new Scanner(System.in);
     while(true)
     {
-        String str=br.readLine();
-        if((str.equals("0")))
-           break; 
-        System.out.println(str);
+        String str=s1.nextLine();
+        pw.println(str);
+        pw.flush();
     
     }
+    }
+}
+class ReadThread extends Thread
+{
+
+    public ReadThread(BufferedReader br) {
+        this.br = br;
+    }
+    BufferedReader br;
+    public void run()
+    {
+        try
+        {
+            while(true)
+            {
+                String str=br.readLine();
+                System.out.println(str);
+            }
+        }
+        catch(Exception ex)
+        {
+            System.out.println(ex);
+        }
     }
 }
--- Game/GameServer.java
+++ Game/GameServer.java
@@ -3,6 +3,10 @@ import java.io.*;
 import java.util.Scanner;
 public class GameServer
 {
+    //*************************************************************************
+ 
+    
+    //*************************************************************************
 public static void main(String[] args) throws IOException
 {
 int portno=1000;
@@ -11,17 +15,55 @@ int num=4;//sc.nextInt();
 System.out.println("hihiiii");
 System.out.println("Connecting");
 ServerSocket ss=new ServerSocket(portno);
+int count=0;
 Socket s=ss.accept();
-        PrintWriter writer=new PrintWriter(s.getOutputStream());  
-        Scanner scanner=new Scanner(System.in);
-        String message="";
-        while(!(message.equals("0")))
-        {
-            System.out.println("Enter Message");
-            message=scanner.nextLine();
-        writer.println(message );
-        writer.println();
-        writer.flush();
-        }
+        PrintWriter writer1=new PrintWriter(s.getOutputStream());  
+        BufferedReader br1=new BufferedReader(new InputStreamReader(s.getInputStream()));
+        s=ss.accept();
+         PrintWriter writer2=new PrintWriter(s.getOutputStream());  
+        BufferedReader br2=new BufferedReader(new InputStreamReader(s.getInputStream()));
+        EchoThread et1=new EchoThread(writer1,writer2,br1);
+       EchoThread et2=new EchoThread(writer1,writer2,br2);
+       et1.start();
+       et2.start();
 }
 }
+
+
+
+   class EchoThread extends Thread
+    {
+        PrintWriter writer1,writer2;
+        BufferedReader br;
+
+    public EchoThread(PrintWriter writer1, PrintWriter writer2, BufferedReader br) {
+        this.writer1 = writer1;
+        this.writer2 = writer2;
+        this.br = br;
+    }
+        
+      @Override
+      public void run()
+      {
+          try
+          {
+          while(true)
+          {
+              String str=br.readLine();
+              writer1.println(str);
+              writer2.println(str);
+              writer1.flush();
+              writer2.flush();
+              
+          }
+          }
+          catch(Exception ex)
+          {
+              System.out.println(ex);
+          }
+      }
+        
+        
+        
+    }
+    
